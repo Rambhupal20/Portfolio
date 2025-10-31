81e6732266b4c4cb1d9be653b981bc3093595053
Rambhupal20 <rambhupalreddyp102@gmail.com>
2025-10-31 11:08:20 +0530

    Fix Resume link URL in Navbar component
    Corrected the URL for the Resume link in the Navbar.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <ul><a href="#projects" >Projects</a></ul>
         <ul><a href="#skills" >Skills</a></ul>
         <ul><a href="#contact" >Contact</a></ul>
-        <ul><a href="https://drive.google.com/file/d/1X0k-wSEIikM_wcXbkQ5NMVefE5JuH98m/view?usp=drive_link" className="nav-link" target='_blank'>Resume</a></ul>
+        <ul><a href="https://drive.google.com/file/d/1X0k-wSEIikM_wcXbkQ5NMVefE5JuH98m/view?usp=sharing"nav-link" target='_blank'>Resume</a></ul>
       </div>
     </div>
   )
